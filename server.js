b8d7fac14b6e46f3b15901196c030c2f4c83eead
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2023-07-01 13:47:10 +0330

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = require('./app');
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
