98f248c8ee1c99a036dc4e8a7cc1955c2d8bf001
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2023-07-31 08:22:29 +0330

    feat: prevent duplicate review on a tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Tour = require("../models/tourModel");
+const AppError = require("../utils/appError");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -34,11 +35,25 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+// reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate([
     // { path: "tour", select: "name" },
     { path: "user", select: "name email" },
   ]);
+  next();
+});
+
+reviewSchema.pre("save", async function (next) {
+  const review = await Review.findOne({ tour: this.tour });
+  if (review)
+    return next(
+      new AppError(
+        "Each user is allowed to post one comment for a specific tour.",
+        405,
+      ),
+    );
   next();
 });
 
