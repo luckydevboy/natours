d5a33f2e67b1f8d6c3e43e7a695b9265bef27aca
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2023-08-05 17:22:11 +0330

    feat: add mapbox to the tour page

--- models/tourModel.js
+++ models/tourModel.js
@@ -116,7 +116,10 @@ const tourSchema = new mongoose.Schema(
 );
 
 tourSchema.index({ slug: 1 });
-tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({
+  price: 1,
+  ratingsAverage: -1,
+});
 tourSchema.index({ startLocation: "2dsphere" });
 
 // Virtual populate
