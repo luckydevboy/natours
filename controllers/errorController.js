4acf2d0efafb5de4349f5b759704a736a4967652
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2023-08-14 23:49:05 +0330

    feat: implement error page

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -88,6 +88,8 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = JSON.parse(JSON.stringify(err));
+    error.message = err.message;
+    
     if (error.name === "CastError") error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === "ValidationError")
